b6f099eca60cdf856da96fed72bca94fd96e828d
Dev Bhalgamiya <146448007+DevBhalgamiya@users.noreply.github.com>
2024-02-15 22:41:23 +0530

    Add files via upload

---
+++ 05_Expressions_operator/index.js
@@ -0,0 +1,57 @@
+// 77;
+// "Harry bhai"
+// false
+console.log("Operators in Js")
+// Arithmetic Operators
+let a = 10;
+let b = 4;
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a / b = ", a/b)
+console.log("a * b = ", a*b)
+console.log("a ** b = ", a**b)
+console.log("a % b = ", a%b)
+// console.log("a++ = ", a++)
+// console.log("a = ", a)
+// console.log("a-- = ", a--)
+// console.log("a = ", a)
+// console.log("++a = ", ++a)
+// console.log("a = ", a)
+// console.log("--a = ", --a)
+// console.log("a = ", a)
+
+// Assignment Operators
+let assignment = 1;
+assignment += 5  // same as asssignment = assignment + 5
+console.log("a is now = ", assignment)
+assignment -= 5  // same as asssignment = assignment - 5
+console.log("a is now = ", assignment)
+assignment *= 5  // same as asssignment = assignment * 5
+console.log("a is now = ", assignment)
+assignment /= 5  // same as asssignment = assignment / 5
+console.log("a is now = ", assignment)
+
+// Comparison Operators
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2", comp1 == comp2)
+console.log("comp1 != comp2", comp1 != comp2)
+console.log("comp1 <= comp2", comp1 <= comp2)
+console.log("comp1 >= comp2", comp1 >= comp2)
+console.log("comp1 < comp2", comp1 < comp2)
+console.log("comp1 > comp2", comp1 > comp2)
+console.log("comp1 === comp2", comp1 === comp2)
+console.log("comp1 !== comp2", comp1 !== comp2)
+
+// Logical Operator
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x!=5)
+console.log(!false)
+
+// Comments
+// This is a Single line comment
+/* This is a multiline comment and a tutorial
+from
+cod with harry*/
