a84f6c1ce3b290ddd70171327b01c7b1c020c37f
Dev Bhalgamiya <146448007+DevBhalgamiya@users.noreply.github.com>
2024-02-17 09:45:53 +0530

    Add files via upload

---
+++ 08_While_Loop/index.js
@@ -0,0 +1,19 @@
+// While Loop
+// let n = prompt("Enter the value of n")
+// n = Number.parseInt(n)
+
+// let i = 0;
+// while(i<n){
+//   console.log(i)
+//   i++;
+// }
+
+// Do-While Loop
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 10;
+do{
+  console.log(i)
+  i++;
+}while(i<n)
