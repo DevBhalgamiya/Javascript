e53977854792f0b3ce2edd534fc841bb86109dee
Dev Bhalgamiya <146448007+DevBhalgamiya@users.noreply.github.com>
2024-02-18 14:05:40 +0530

    Add files via upload

---
+++ 11_String_Method/index.js
@@ -0,0 +1,23 @@
+let name = "CherryBhai"
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2, 4))
+// console.log(name.slice(2))
+// console.log(name.replace("Bhai", "Bhau"))
+
+// let friend = "Naman"
+// console.log(name.concat(" is a friend of ", friend, " Ok "))
+// let friend2 = "       Meena       "
+// console.log(friend2)
+// console.log(friend2.trim())
+
+let fr = "Raman" + "Shivika" + "Cherry"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr[4])
+// fr[5] = "o"  // This is not possible
+
+console.log(fr)
