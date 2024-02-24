87207e26e58ab5cde0ef12f534172f71629fcc74
DevBhalgamiya <devbhalgamiya18@gmail.com>
2024-02-24 14:39:43 +0530

    commit

---
+++ 03_var_let_const/index.js
@@ -0,0 +1,14 @@
+console.log("Javascript tutorial 3: var, let and const")
+// var a = 45;
+// var a = "p";
+let b = "Dev";
+const author = "Dev"
+// let author = 5 // Throws an error because constant cannot be changed
+b = 4
+let c = null
+let d = undefined
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
